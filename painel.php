a95737159a06826b8cb8b0222391d86b189c88fa
JaoMan <61010809+JaoMan@users.noreply.github.com>
2020-02-27 08:41:59 -0300

    painel.php

---
+++ painel.php
@@ -0,0 +1,20 @@
+<DOCTYPE html>
+<html>
+<?php
+echo "Olá, mundo!";
+?>
+ <head>
+	<title>Meu site 2 </title>
+ <head>
+  <body>
+	<header>
+	
+	<div style="background-color:blue;">
+		<h2 style="color:white;"> QUEBRA TUDO NESSA MERDA MESMO</>	
+	</div>	
+
+	<header>
+	<p>Este é meu paragrafo<p>	
+
+  </body>
+</html>
